94fbb170aef2727915bdecf35e67b9a918860211
moe <moe.lak20@gmail.comgit config --global user.email moe.lak20@gmail.com>
2020-10-03 21:53:09 -0700

    adding the code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Double Burgure');
+INSERT INTO burgers (name, devoured) VALUES ('Hamburgur', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Burgure', true);
